2c85843b7c3011b8adb4a0737de89717d3074b66
ega-erinovian <erinovian69@gmail.com>
2024-12-09 15:35:05 +0700

    feat: forgot-password

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import {
+  forgotPasswordController,
   loginController,
   registerController,
 } from "../controllers/auth.controller";
-import { validateLogin, validateRegister } from "../validators/validator";
+import {
+  validateForgotPassword,
+  validateLogin,
+  validateRegister,
+} from "../validators/validator";
 
 const router = Router();
 
 router.post("/register", validateRegister, registerController);
 router.post("/login", validateLogin, loginController);
+router.post(
+  "/forgot-password",
+  validateForgotPassword,
+  forgotPasswordController
+);
 
 export default router;
