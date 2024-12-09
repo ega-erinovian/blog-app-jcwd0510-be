da6033270d6a5676fed3c7b4b82df7ef872274d4
ega-erinovian <erinovian69@gmail.com>
2024-12-09 11:04:43 +0700

    feat: login

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { registerController } from "../controllers/auth.controller";
-import { validateRegister } from "../validators/validator";
+import {
+  loginController,
+  registerController,
+} from "../controllers/auth.controller";
+import { validateLogin, validateRegister } from "../validators/validator";
 
 const router = Router();
 
 router.post("/register", validateRegister, registerController);
+router.post("/login", validateLogin, loginController);
 
 export default router;
